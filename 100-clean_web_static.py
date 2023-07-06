3a4742c55beb4dfb3260ac3e076b64af81bafe8e
kazzonline <azeezolatunji02@gmail.com>
2023-07-06 03:01:47 -0700

    all tasks

---
+++ 100-clean_web_static.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+""" A function that deploys """
+from fabric.api import *
+
+
+env.hosts = ['52.73.253.246', '100.26.243.162']
+env.user = "ubuntu"
+
+
+def do_clean(number=0):
+    """ CLEANS """
+
+    number = int(number)
+
+    if number == 0:
+        number = 2
+    else:
+        number += 1
+
+    local('cd versions ; ls -t | tail -n +{} | xargs rm -rf'.format(number))
+    path = '/data/web_static/releases'
+    run('cd {} ; ls -t | tail -n +{} | xargs rm -rf'.format(path, number))
